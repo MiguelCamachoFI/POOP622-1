7cc4adeddd57d2acecf4e8e6fad3a71979ebca13
Miguel Angel Camacho Garduño <41399255+MiguelCamachoFI@users.noreply.github.com>
2021-10-21 08:36:57 -0500

    Creación del proyecto

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.mycompany.poop6ide","c":"Main","l":"Main()","url":"%3Cinit%3E()"},{"p":"com.mycompany.poop6ide","c":"Main","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"com.mycompany.poop6ide","c":"Main","l":"suma(int, int)","url":"suma(int,int)"}]
